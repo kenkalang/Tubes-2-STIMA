2399f232085aef54a73eb095979f16294e257b9d
unknown <kenkalang@yahoo.com>
2022-03-18 16:20:22 +0700

    benerin

---
+++ foldercrawling/DFS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace cariDFS
+{
+    internal class pencarianDFS
+    {
+        public static void DFS(string kemungkinan, string dicari, string path)
+        {
+            var isi = Directory.GetFiles(path, "*.*");
+            foreach (string file in isi)
+            {
+                if (Path.GetFileName(file) == dicari)
+                {
+                    if (kemungkinan == "N")
+                    {
+                        Console.WriteLine(Path.GetFullPath(file) + " KETEMU JEMBUOTTTTTTT");
+                        System.Environment.Exit(0);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(Path.GetFullPath(file) + " KETEMU JEMBUOTTTTTTT");
+                        continue;
+                    }
+                }
+                else
+                {
+
+                    Console.WriteLine(file);
+                }
+            }
+
+            var folder = Directory.GetDirectories(path);
+
+            foreach (string subfolder in folder)
+            {
+                Console.WriteLine(Path.GetFullPath(subfolder));
+                DFS(kemungkinan, dicari, subfolder);
+
+            }
+
+        }
+    }
+}
---
+++ foldercrawling/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using cariBFS;
+using cariDFS;
+
+namespace foldercrawling
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pilih directory : ");
+            string rootPath = @Console.ReadLine();
+
+            Console.WriteLine("Masukkan file : ");
+            string dicari = Console.ReadLine();
+
+            Console.WriteLine("Cari semua kemungkinan? (Y/N)");
+            string kemungkinan = Console.ReadLine();
+
+
+            Console.WriteLine("Metode digunakan : (ketik 1 atau 2)");
+            Console.WriteLine("1. DFS ");
+            Console.WriteLine("2. BFS ");
+            string metode = Console.ReadLine();
+            // string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            //  Console.WriteLine("Hello World!");
+
+
+            var files = Directory.GetFiles(rootPath, "*.*");
+
+            Queue<string> pencarian = new Queue<string>();
+
+            if (metode == "1")
+                pencarianDFS.DFS(kemungkinan, dicari, rootPath);
+            else if (metode == "2")
+                pencarianBFS.BFS(kemungkinan, dicari, rootPath);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+
+        }
+
+    }
+}
